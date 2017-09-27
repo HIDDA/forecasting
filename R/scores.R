f88289a05592f64eed527d36f642df04aae6e146
Sebastian Meyer <seb.meyer@fau.de>
2017-09-27 10:25:26 +0200

    add logs_lnorm()

--- R/scores.R
+++ R/scores.R
@@ -1,5 +1,5 @@
 ################################################################################
-### Proper scoring rules
+### Proper Scoring Rules for Log-Normal Forecasts
 ###
 ### Copyright (C) 2017 Sebastian Meyer
 ###
@@ -9,13 +9,11 @@
 ### https://www.R-project.org/Licenses/.
 ################################################################################
 
-##' Dawid-Sebastiani Score (DSS) for Log-Normal Predictions
+##' Proper Scoring Rules for Log-Normal Forecasts
 ##'
-##' Computes the DSS for the log-normal distribution similar to
-##' [surveillance::dss()] for the Poisson and negative binomial distribution.
 ##' Predictions are assumed to have a [LN][plnorm](`meanlog`, `sdlog`)
-##' distribution. The function is vectorized and preserves the dimensions of the
-##' input.
+##' distribution. The functions are vectorized and preserve the dimensions of
+##' the input.
 ##'
 ##' @param x the observed counts.
 ##' @param meanlog,sdlog parameters of the log-normal distribution, i.e., mean
@@ -23,12 +21,30 @@
 ##' @return scores for the predictions of the observations in `x` (maintaining
 ##'     their dimensions).
 ##' @author Sebastian Meyer
-##' @export
+##' @seealso The R package \CRANpkg{scoringRules} implements the logarithmic
+##'     score and the continuous ranked probability scores (CRPS) for many
+##'     continuous distributions.
+##' @name scores_lnorm
+NULL
 
+##' @describeIn scores_lnorm Computes the Dawid-Sebastiani score (DSS) for the
+##'     log-normal distribution similar to [surveillance::dss()] for the Poisson
+##'     and negative binomial distribution.
+##' @export
 dss_lnorm <- function (x, meanlog, sdlog)
 {
     varlog <- sdlog^2
     surveillance:::.dss(meanP = exp(meanlog + varlog/2),
                         varP = exp(2*meanlog + varlog) * expm1(varlog),
                         x = x)
+}
+
+##' @describeIn scores_lnorm Computes the logarithmic score (LogS) for the
+##'     log-normal distribution similar to [surveillance::logs()] for the Poisson
+##'     and negative binomial distribution.
+##' @importFrom stats dlnorm
+##' @export
+logs_lnorm <- function (x, meanlog, sdlog)
+{
+    -dlnorm(x = x, meanlog = meanlog, sdlog = sdlog, log = TRUE)
 }
