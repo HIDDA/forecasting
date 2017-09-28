835fc5674bd0bda56bbdc90c362c954149b62374
Sebastian Meyer <seb.meyer@fau.de>
2017-09-28 17:45:04 +0200

    long-term forecasts for all three periods with hhh4

--- vignettes/setup.R
+++ vignettes/setup.R
@@ -4,8 +4,7 @@ library("HIDDA.forecasting")
 .T <- match(paste0(2013:2015, "-12"), strftime(index(CHILI), "%Y-%m"))
 index(CHILI)[.T]
 TEST <- lapply(.T, function (T) seq(from = T, by = 1, length.out = 30))
-TRAIN <- lapply(TEST, function (test) 1:(test[1]-1))
 
 ## at which time points to compute one-week-ahead forecasts
-OWA <- tail(TRAIN[[1]],1):(length(CHILI)-1)
+OWA <- (TEST[[1]][1]-1):(length(CHILI)-1)
 length(OWA)
