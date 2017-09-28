23d0c574feaed65983630bb9f2e1550035bd0950
Sebastian Meyer <seb.meyer@fau.de>
2017-09-28 11:11:38 +0200

    add update.Arima()

---
+++ R/update.Arima.R
@@ -0,0 +1,39 @@
+################################################################################
+### Refit an ARIMA Model on a Subset of the Time Series
+###
+### Copyright (C) 2017 Sebastian Meyer
+###
+### This file is part of the R package "HIDDA.forecasting",
+### free software under the terms of the GNU General Public License, version 2,
+### or (at your option) any later version, a copy of which is available at
+### https://www.R-project.org/Licenses/.
+################################################################################
+
+##' Refit an ARIMA Model on a Subset of the Time Series
+##'
+##' There seems to be no function in package \CRANpkg{forecast} (as of version
+##' 8.2) to re-estimate an ARIMA model on a subset of the original time series.
+##' This `update` method does exactly that.
+##' @param object an object of class `"Arima"`, e.g., from
+##'     [forecast::auto.arima()].
+##' @param subset an integer vector selecting part of the original time series
+##'     (and external regressors).
+##' @param ... further arguments to be passed to [arima()].
+##' @return the updated model.
+##' @author Sebastian Meyer
+##' @importFrom stats update
+##' @export
+
+update.Arima <- function (object, subset, ...) {
+    x <- object$x
+    if (!missing(subset))
+        x <- x[subset]
+    ordseas <- object$arma[c(1, 6, 2, 3, 7, 4, 5)]  # see 'arimaorder'
+    forecast::Arima(x,
+        order = ordseas[1:3],
+        seasonal = list(order = ordseas[4:6], period = ordseas[7]),
+        xreg = object$xreg[subset,,drop=FALSE],
+        include.mean = "intercept" %in% names(object$coef),
+        include.drift = "drift" %in% names(object$coef),
+        lambda = object$lambda, ...)
+}
