8d272a83a4bbc97723e4de9a85c26496c429655b
Sebastian Meyer <seb.meyer@fau.de>
2017-09-29 09:27:24 +0200

    cosmetics

--- vignettes/setup.R
+++ vignettes/setup.R
@@ -8,3 +8,8 @@ TEST <- lapply(.T, function (T) seq(from = T, by = 1, length.out = 30))
 ## at which time points to compute one-week-ahead forecasts
 OWA <- (TEST[[1]][1]-1):(length(CHILI)-1)
 length(OWA)
+
+## auxiliary function for annotation
+format_period <- function (index, fmt = "%Y-W%V", collapse = " to ") {
+    paste0(strftime(index(CHILI)[range(index)], fmt), collapse = collapse)
+}
