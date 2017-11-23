171b85955d282db73b69ef157e257e6c576fb184
Sebastian Meyer <seb.meyer@fau.de>
2017-11-23 15:53:16 +0100

    use optipng in package vignettes (based on environment variable)

--- R/zzz.R
+++ R/zzz.R
@@ -10,3 +10,22 @@ zoo::autoplot.zoo
 #' @importFrom zoo fortify.zoo
 #' @export
 zoo::fortify.zoo
+
+
+#' Should the Package Vignettes Use `optipng`?
+#'
+#' If the environment variable \env{OPTIPNG} is set to `TRUE` (or
+#' something regarded as such via [as.logical()]), the png files produced
+#' when building the vignettes will be compressed (losslessly) using the
+#' \command{optipng} program.
+#'
+#' @param level the optimization level (0-7). The default (2) is a good
+#'     compromise between compression and speed.
+#' @keywords internal
+use_optipng <- function (level = 2)
+{
+    if (isTRUE(as.logical(Sys.getenv("OPTIPNG")))) {
+        knitr::knit_hooks$set(optipng = knitr::hook_optipng)
+        paste0("-o", level)
+    } else NULL
+}
